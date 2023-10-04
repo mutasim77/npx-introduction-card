5f673d9e811324754656c4546566a4b04bd61138
 <96326525+mutasim77@users.noreply.github.com>
2023-10-04 11:24:40 +0600

    💾 Added: Information file

---
+++ data.js
@@ -0,0 +1,16 @@
+import chalk from "chalk";
+
+const data = {
+    name: chalk.bold.green("                  Hey, I'm Mutasim"),
+    fact: chalk.yellow('I can type faster than I can talk!'),
+    twitter: chalk.hex('#00A1D9')("https://twitter.com/mvtasim77"),
+    github: chalk.hex('#54ff73')(" https://github.com/mutasim77"),
+    website: chalk.hex('#00AB9E')("https://mutasim.top"),
+
+    labelFact: chalk.hex('#FF6262').bold("      Fun Fact:"),
+    labelTwitter: chalk.hex('#629DFF').bold("       🐦 Twitter:"),
+    labelGitHub: chalk.hex('#54ff73').bold("       🦄 GitHub:"),
+    labelWebsite: chalk.hex('#59FFC8').bold("       🌐 Website:"),
+};
+
+export default data;
